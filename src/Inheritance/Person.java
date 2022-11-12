04f1e21276ff2f7b0839e118e03ee47bfa663de0
kenjikuroki <90098303+kenjikuroki@users.noreply.github.com>
2022-11-12 11:07:17 +0900

    inheritance

---
+++ src/Inheritance/Person.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Person{
+    private String name;
+
+    public void setName(String n){
+        name = n;
+    }
+
+public void display(){
+            System.out.print("名前：" + name);
+        }
+    }
---
+++ src/Inheritance/StuSample.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+public class StuSample{
+    public static void main(String[] args){
+        Student stu = new Student();
+
+        //スーパークラスのメソッド
+        stu.setName("菅原");
+        stu.display();
+
+        //サブクラスのメソッド
+        stu.setStuNo(1);
+        stu.displayStuNo();
+    }
+}
